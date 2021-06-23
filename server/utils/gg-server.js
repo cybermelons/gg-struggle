7360d7fb3bd30a7c7501f182ba4dc75b0b60fcc9
cybermelon <cybermelon@yeet.st>
2021-06-23 21:22:59 +0000

    start using axios

--- server/utils/gg-server.js
+++ server/utils/gg-server.js
@@ -1,4 +1,5 @@
 const https = require('https')
+const https = require('axios')
 
 
 const ggServerPost = async ({ headers, body, slug, method }) => {
@@ -7,23 +8,28 @@ const ggServerPost = async ({ headers, body, slug, method }) => {
     //hostname: 'ggst-game.guiltygear.com',
     port: 443,
     method: method,
-    path: `api/${slug.join('/')}`
+    path: `api/${slug.join('/')}`,
+    headers: {
+      'user-agent': 'Steam',
+      'content-type': 'application/x-www-form-urlencoded',
+    }
   }
 
   console.log(`pinging ggst`)
+  console.log({options})
 
   const payload = new Promise((resolve, reject) => {
     // TODO log failed responses
 
     const req = https.request(options, (res) => {
       console.log(`Setting up req to gg server`)
+      console.log({res})
 
       res.on('data', d => {
         // return the body
         resolve({ status: res.statusCode, headers: {}, body: d})
       })
     })
-    req.setHeader('user-agent', 'Steam')
 
     req.on('error', (e) => {
       console.error(`Request to gg server failed`);
@@ -31,6 +37,7 @@ const ggServerPost = async ({ headers, body, slug, method }) => {
       reject({ status: 505, headers: {}, body: options})
     })
 
+    req.write(body.data)
     req.end()
   })
 
