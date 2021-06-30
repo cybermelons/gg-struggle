06f2ae054753b6ab09cdb0687b0cc5afef061355
cybermelon <cybermelon@yeet.st>
2021-06-30 18:39:55 +0000

    generates p12

--- tools/genkey.sh
+++ tools/genkey.sh
@@ -12,3 +12,5 @@ openssl req -sha256 -nodes -newkey rsa:2048 -keyout localhost_key.pem -out local
 # Create cert aqnd sign with CA
 echo "Creasting cert and siginin with CA"
 openssl x509 -req -days 398 -in localhost.csr -CA CA_cert.pem -CAkey CA_key.pem -CAcreateserial -out localhost_cert.pem -extensions req_ext -extfile localhost.cnf
+
+openssl pkcs12 -export -out ggwin.p12 -inkey localhost_key.pem -in localhost_cert.pem
