c70d59f475e2d7161f22e988dbb27b8d4f238a24
supika <supika9@pm.me>
2021-06-30 22:14:49 +0000

    Update 'tools/gg-struggle-install.ps1'

--- tools/gg-struggle-install.ps1
+++ tools/gg-struggle-install.ps1
@@ -14,16 +14,16 @@ function Install-GgstProxy {
     {
         Write-Error "Failed to connect to host $proxyIp"
     }
-
+    
     # Install Cert
     $p12 = new-object System.Security.Cryptography.X509Certificates.X509Certificate2
     $certPath = "./ggwin.p12" # TODO: this is if you're in the same directory, but we can resolve this later
     $pfxPass = ""
     $p12.Import($certPath,$pfxPass,"Exportable,PersistKeySet")
-
+    
     $store = New-Object System.Security.Cryptography.X509Certificates.X509Store([System.Security.Cryptography.X509Certificates.StoreName]::Root,"LocalMachine")
     $store.Open("MaxAllowed")
-    $store.Add($pfx)
+    $store.Add($p12)
     $store.Close()
 }
 
@@ -34,28 +34,28 @@ function Uninstall-GgstProxy
 
     # Uninstall Cert
     $p12 = new-object System.Security.Cryptography.X509Certificates.X509Certificate2
-    $certPath = "./ggwin.p12" # this is if you're in the same directory, but we can resolve this later #TODO
+    $certPath = "./ggwin.p12" # TODO: this is if you're in the same directory, but we can resolve this later
     $pfxPass = ""
     $p12.Import($certPath,$pfxPass,"Exportable,PersistKeySet")
-
+    
     $store = New-Object System.Security.Cryptography.X509Certificates.X509Store([System.Security.Cryptography.X509Certificates.StoreName]::Root,"LocalMachine")
-    $store.Open("MaxAllowed")
+    $store.Open("MaxAllowed") 
     $store.Remove($p12)
     $store.Close()
 }
 
-
-while {
+$end = $false;
+while (!($end)) {
     Write-Host '1. Install GgstProxy';
     Write-Host '2. Uninstall GgstProxy';
     Write-Host '3. Exit';
     $input = Read-Host
-
+    
     switch($input)
     {
         "1" {Install-GgstProxy}
         "2" {Uninstall-GgstProxy}
-        "3" {break}
+        "3" {$end = $true}
     }
 }
 
