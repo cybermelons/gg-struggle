36923b88183f4b6294c4e941a35163c14c885a9c
cybermelon <cybermelon@yeet.st>
2021-07-05 03:43:15 -0400

    fucking finally openssl works

--- tools/gencert.ps1
+++ tools/gencert.ps1
@@ -1,5 +1,22 @@
 # NOTE: This must be run as admin!
 
+# function defs
+function Enable-Read {
+  param (
+    [string[]]$FilePath
+  )
+
+  # https://stackoverflow.com/questions/25779423/powershell-to-set-folder-permissions
+  $Acl = Get-Acl $FilePath
+  #$everyone = new SecurityIdentifier(WellKnownSidType.WorldSid, null)
+  $Ar = New-Object System.Security.AccessControl.FileSystemAccessRule( "Everyone", "Read", "Allow")
+
+  $Acl.SetAccessRule($Ar)
+  Set-Acl $FilePath $Acl
+  Write-Host "Enabled read permissions on $FilePath"
+}
+
+# entry point
 if (($args.Count -lt 1) -or !(Test-Path $args[0])) {
   Write-Host "Usage: gencert.ps1 <outputDir>"
   Exit 5
@@ -12,40 +29,6 @@ $hash = Get-Random
 $tmpDir = "$Env:TEMP\$hash"
 $keyDst = "$outputDir\gg-struggle-key.pem"
 $certDst = "$outputDir\gg-struggle-cert.pem"
-
-
-#function Gen-Cert {
-#  param (
-#    [string] $Key,
-#    [string] $Cert
-#  )
-#
-#  $cert2=Get-ChildItem Cert:\LocalMachine\my |
-#  Where-Object { $_.FriendlyName -match $friendlyName }
-#
-# # $path = ‘cert:\localMachine\my\’ + $cert2.thumbprint
-#  Export-Certificate -cert $genCert -FilePath $tmpDir\cert.der -type CERT –noclobber | Out-Null
-#  certutil -encode $tmpDir\cert.der $Cert | Out-Null
-#}
-#
-#$Env:OPENSSL_CONF="$Env:ProgramFiles\OpenSSL-Win64\bin\openssl.cfg"
-$Env:PATH="$Env:PATH;$Env:ProgramFiles\OpenSSL-Win64\bin"
-
-if (-Not (Get-Command openssl -errorAction SilentlyContinue)) {
-  Write-Error "openssl is not found on PATH environment variable: $Env:Path"
-  Write-Error "Can't generate key without openssl. Please install openssl-64 from https://slproweb.com/products/Win32OpenSSL.html . Aborting."
-  Exit 1
-}
-
-openssl req `
-  -x509 `
-  -sha256 `
-  -nodes `
-  -days 365 `
-  -newkey rsa:2048 `
-  -keyout $keyDst `
-  -out $certDst `
-  -config localhost.cnf
 
 function Enable-Read {
   param (
@@ -62,21 +45,39 @@ function Enable-Read {
   Write-Host "Enabled read permissions on $FilePath"
 }
 
+#$Env:OPENSSL_CONF="$Env:programfiles\OpenSSL-Win64\bin\openssl.cfg"
+$Env:PATH="$Env:PATH;$Env:programfiles\OpenSSL-Win64\bin"
 
-# https://stackoverflow.com/questions/65083411/creating-pem-file-through-powershell
+# i'm tired. just do a brute-force for openssl binaries
+$openssl32_1 = "$Env:programfiles\OpenSSL-Win32\bin"  # C:\Program Files (x86)
+$openssl32_2 = "$Env:ProgramW6432\OpenSSL-Win32\bin"  # C:\Program Files
+$openssl64_1 = "$Env:programfiles\OpenSSL-Win64\bin"  # C:\Program Files (x86)
+$openssl64_2 = "$Env:ProgramW6432\OpenSSL-Win64\bin"  # C:\Program Files
 
+$Env:PATH="$Env:PATH;" + `
+            ";$openssl32_1" + `
+            ";$openssl32_2" + `
+            ";$openssl64_1" + `
+            ";$openssl64_2"
 
-# Generate self-signed cert, and key
-# Move key to output directory as "gg-struggle.key"
+if (-Not (Get-Command openssl -errorAction SilentlyContinue)) {
+  Write-Error "openssl is not found on PATH environment variable: $Env:Path"
+  Write-Error "Can't generate key without openssl. Please install openssl-64 from https://slproweb.com/products/Win32OpenSSL.html . Aborting."
+  Read-Host -Prompt "Press Enter to exit"
+  Exit 69
+}
 
-# Install cert from the My store into Root store
-#$certId = $cert.Thumbprint
-#Move-Item -path $storeMy\$certId -Destination $storeRoot
-#Write-Host "Installed $storeRoot\$certId to $storeRoot "
+# openssl must be installed on user's computer
+openssl req `
+  -x509 `
+  -sha256 `
+  -nodes `
+  -days 365 `
+  -newkey rsa:2048 `
+  -keyout $keyDst `
+  -out $certDst `
+  -config localhost.cnf
 
-$CertStore = New-Object System.Security.Cryptography.X509Certificates.X509Store  -ArgumentList  "Cert:", "LocalMachine"
-
-$CertStore.Open('ReadWrite')
 Import-Certificate -FilePath "$certDst" -CertStoreLocation "$storeRoot"
 
 Enable-Read $certDst
