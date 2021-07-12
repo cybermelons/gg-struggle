e53373830e5cc3c3cacc14b47c3cfff12ede86c3
cybermelon <cybermelon@yeet.st>
2021-07-12 15:19:04 -0400

    ok

--- server/update.js
+++ server/update.js
@@ -1,16 +1,20 @@
 //degit git@git.yeet.st:cybermelon/gg-struggle
 const degit = require('degit');
 
-const emitter = degit('tsaibermelon/gg-struggle', {
+const emitter = degit('tsaibermelon/gg-struggle#main', {
     cache: true,
     force: true,
     verbose: true,
 });
 
 emitter.on('info', info => {
+    console.error(info.message);
+});
+
+emitter.on('info', info => {
     console.log(info.message);
 });
 
-emitter.clone('..').then(() => {
+emitter.clone('tmp').then(() => {
     console.log('done');
 });
