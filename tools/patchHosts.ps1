dfa42fffc99951b5e524041d35ddddd23d17135a
cybermelon <cybermelon@yeet.st>
2021-07-13 17:18:49 -0400

    d

--- tools/patchHosts.ps1
+++ tools/patchHosts.ps1
@@ -3,7 +3,14 @@
 # 1. The game connects to gg-struggle via "ggst-game.guiltygear.com"
 # 2. We can still connect to arcsys via "ggst-game-real.guiltygear.com"
 
-Install-Module PsHosts  # makes it easy to edit hosts file
+# Install PsHosts for usability
+if (Get-Module -ListAvailable -Name PsHosts) {
+  Write-Host "PsHosts already installed. Continuing..."
+}
+else {
+  Write-Host "PsHosts does not exist. Installing..."
+  Install-Module PsHosts  # makes it easy to edit hosts file
+}
 
 $ggDummyName = "ggst-game-real.guiltygear.com"
 $ggHostName = "ggst-game.guiltygear.com"
@@ -13,6 +20,7 @@ $dns = Resolve-DnsName -NoHostsFile "$ggHostName"
 # add routes for arcsys
 for ($i = 0; $i < $dns.length; $i++) {
   $address = $dns[$i].Address
+  Write-Host "Adding $address $ggDummyName"
   Add-HostEntry "$ggDummyName" $address
 }
 
--- tools/rmHosts.ps1
+++ tools/rmHosts.ps1
@@ -2,6 +2,12 @@
 #
 # This script removes any references to "...guiltygear.com"
 
-Install-Module PsHosts  # makes it easy to edit hosts file
+if (Get-Module -ListAvailable -Name PsHosts) {
+  Write-Host "PsHosts already installed. Continuing..."
+}
+else {
+  Write-Host "PsHosts does not exist. Installing..."
+  Install-Module PsHosts  # makes it easy to edit hosts file
+}
 
 Remove-HostEntry *.guiltygear.com
